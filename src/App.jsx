c948cd7d6fdb4ce02693df0cee2da1174b036d6e
Rossi Doyle <rossidoyle0@gmail.com>
2026-03-27 22:26:25 -0500

    added food cards

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,20 @@ function App() {
       <main className="main">
         <section id="home" className="hero">
         </section>
+
+        <section className="cards-section">
+          <div className="cards-container">
+            <div className="card" id="card-one">
+              <h3 id="card-title">PERFECT PASTA</h3>
+            </div>
+            <div className="card" id="card-two">
+              <h3 id="card-title">HANDTOSSED PIZZA</h3>
+            </div>
+            <div className="card" id="card-three">
+              <h3 id="card-title">AUTHENTIC DESSERTS</h3>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   )
